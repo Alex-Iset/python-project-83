951b5e6bd358a93a6c884e40ba0654077b52153e
Alex Iset <fl3shtrader@yandex.ru>
2025-05-13 19:16:33 +0300

    fixed: small fixes

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -1,14 +1,10 @@
 import validators
 
 
-def is_valid_url(url):
-    return validators.url(url)
-
-
 def validate(url):
-    errors = {}
     if not url:
-        errors['url'] = 'Поле URL не заполнено'
-    if not is_valid_url(url) and len(url) < 255:
-        errors['url'] = 'Некорректный URL'
-    return errors
+        return 'Поле URL не заполнено'
+    if not validators.url(url):
+        return 'Некорректный URL'
+    if len(url) > 255:
+        return 'URL не должен превышать 255 символов'
