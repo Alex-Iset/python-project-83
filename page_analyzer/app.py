bc6f62d00a8f173ef6d2e212a2264a66c39d4cba
Alex Iset <fl3shtrader@yandex.ru>
2025-05-14 16:14:01 +0300

    fixed: small fixes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -54,7 +54,7 @@ def url_post():
     errors = validate(url)
     if errors:
         flash(errors, 'danger')
-        return render_template('urls/index.html'), 422
+        return redirect('/')
     repo = DataBase(get_db(DATABASE_URL))
     norm_url = canonicalize_url(url).rstrip('/')
     exist_url = repo.get_url(norm_url)
