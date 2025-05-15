743ee9ce9ed4277b0284b6bcdac3562a8cffebfd
Alex Iset <fl3shtrader@yandex.ru>
2025-05-15 14:04:33 +0300

    fixed: edits for SolarQube

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -10,7 +10,7 @@ def get_db(db_url):
     return g.db
 
 
-def close_db(exception=None):
+def close_db():
     db = g.pop('db', None)
     if db is not None:
         db.close()
@@ -21,91 +21,76 @@ class DataBase:
         self.conn = conn
 
     def save_url(self, url):
-        try:
-            with self.conn.cursor() as cur:
-                cur.execute(
-                    "INSERT INTO urls (name) VALUES (%s) RETURNING id",
-                    (url,),
-                )
-                id = cur.fetchone()[0]
-                return id
-        except Exception:
-            raise
+        with self.conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO urls (name) VALUES (%s) RETURNING id",
+                (url,),
+            )
+            url_id = cur.fetchone()[0]
+            return url_id
+
 
     def find_url(self, id):
-        try:
-            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
-                row = cur.fetchone()
-                return dict(row) if row else None
-        except Exception:
-            raise
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+            row = cur.fetchone()
+            return dict(row) if row else None
+
 
     def get_url(self, url):
-        try:
-            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls WHERE name = %s", (url,))
-                return cur.fetchone()
-        except Exception:
-            raise
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE name = %s", (url,))
+            return cur.fetchone()
+
 
     def get_urls(self):
-        try:
-            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls ORDER BY id DESC")
-                return [dict(row) for row in cur]
-        except Exception:
-            raise
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT * FROM urls ORDER BY id DESC")
+            return [dict(row) for row in cur]
+
 
     def save_check(self, url_id, status_code, h1, title, description):
-        try:
-            with self.conn.cursor() as cur:
-                cur.execute(
-                    """
-                    INSERT INTO url_checks (
-                        url_id,
-                        status_code,
-                        h1,
-                        title,
-                        description
-                    ) VALUES (%s, %s, %s, %s, %s)
-                    """,
-                    (url_id, status_code, h1, title, description),
-                )
-        except Exception:
-            raise
+        with self.conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO url_checks (
+                    url_id,
+                    status_code,
+                    h1,
+                    title,
+                    description
+                ) VALUES (%s, %s, %s, %s, %s)
+                """,
+                (url_id, status_code, h1, title, description),
+            )
+
 
     def get_checks(self, url_id):
-        try:
-            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute(
-                    """
-                    SELECT *
-                    FROM url_checks
-                    WHERE url_id = %s
-                    ORDER BY id DESC
-                    """,
-                    (url_id,)
-                )
-                return [dict(row) for row in cur]
-        except Exception:
-            raise
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                """
+                SELECT *
+                FROM url_checks
+                WHERE url_id = %s
+                ORDER BY id DESC
+                """,
+                (url_id,)
+            )
+            return [dict(row) for row in cur]
+
 
     def get_last_check(self, url_id):
-        try:
-            with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute(
-                    """
-                    SELECT 
-                        created_at, 
-                        status_code
-                    FROM url_checks
-                    WHERE url_id = %s
-                    ORDER BY created_at DESC
-                    LIMIT 1
-                    """,
-                    (url_id,)
-                )
-                return cur.fetchone()
-        except Exception:
-            raise
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                """
+                SELECT 
+                    created_at, 
+                    status_code
+                FROM url_checks
+                WHERE url_id = %s
+                ORDER BY created_at DESC
+                LIMIT 1
+                """,
+                (url_id,)
+            )
+            return cur.fetchone()
